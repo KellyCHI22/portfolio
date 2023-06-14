7dbf8a7d126db0cde56b2d6f31b8f1b5b204f7ef
Hsin-tingCHI <chihsinting@gmail.com>
2023-06-14 14:19:16 +0200

    layout: add ThemeToggle button and change favicon according to theme

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,34 @@
+// reference: https://www.kevinzunigacuellar.com/blog/dark-mode-in-astro/
+
+import { useEffect, useRef, useState } from 'react';
+import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'light');
+  // change favicon according to theme change
+  const faviconRef = useRef(document.querySelector('link[rel="icon"]'));
+
+  const handleClick = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+      faviconRef.current.setAttribute('href', `/public/logo-light.svg`);
+    } else {
+      document.documentElement.classList.remove('dark');
+      faviconRef.current.setAttribute('href', `/public/logo-dark.svg`);
+    }
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <button
+      onClick={handleClick}
+      className="p-2 bg-jet-500 rounded-full text-alabaster-500 dark:bg-alabaster-500 dark:text-jet-500"
+    >
+      {theme === 'light' ? <BsFillMoonStarsFill /> : <BsFillSunFill />}
+    </button>
+  );
+}
