4430331a34c5d809ed93731504652b59863119eb
Hsin-tingCHI <chihsinting@gmail.com>
2023-06-15 13:44:13 +0200

    layout: add ProjectsSection and ImageSlider component (and image assets)

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,71 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
+
+// const imagesData = [
+//   '/public/mockup_alpha_twitter_1.jpg',
+//   '/public/mockup_howto_1.jpg',
+//   '/public/monday_blue.jpg',
+// ];
+
+export default function ImageSlider({ imagesData }: { imagesData: string[] }) {
+  const [current, setCurrent] = useState(0);
+  const length = imagesData.length;
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(imagesData) || imagesData.length <= 0) {
+    return null;
+  }
+
+  return (
+    <div className="relative">
+      <div className="overflow-hidden">
+        <div
+          className="flex transition-transform ease-out duration-500 w-full h-48"
+          style={{ transform: `translateX(-${current * 100}%)` }}
+        >
+          {imagesData.map((image, index) => {
+            return (
+              <img
+                key={index}
+                src={image}
+                alt="project-image"
+                className="w-full object-cover flex-shrink-0"
+              />
+            );
+          })}
+        </div>
+      </div>
+      <button
+        className="p-[0.5] bg-jet-500 text-alabaster-500 rounded-full absolute left-2 top-[40%] grid place-items-center opacity-70 hover:opacity-100"
+        onClick={prevSlide}
+      >
+        <RiArrowLeftSLine className="text-2xl" />
+      </button>
+      <button
+        className="p-[0.5] bg-jet-500 text-alabaster-500 rounded-full absolute right-2 top-[40%] grid place-items-center opacity-70 hover:opacity-100"
+        onClick={nextSlide}
+      >
+        <RiArrowRightSLine className="text-2xl" />
+      </button>
+      <div className="flex gap-3 w-full justify-center mt-3">
+        {Array.from(imagesData).map((image, index) => {
+          return (
+            <div
+              key={index}
+              className={clsx('w-4 h-1 rounded-full bg-jet-200', {
+                'bg-jet-500': current === index,
+              })}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
